cf5d6083b37f5bdb3326ca4189b5fb10104261ad
parsTroy <troyparsons@hotmail.com>
2022-09-18 10:07:45 -0400

    fixed chart

--- src/components/Main/StockChart/StockChart.jsx
+++ src/components/Main/StockChart/StockChart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import styles from './StockChart.module.css';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, AreaChart, Area } from 'recharts';
 import { fetchHistoricalData } from '../../../api/api';
 import StockContext from '../../../context/StockContext';
 import ChartFilter from '../ChartFilter/ChartFilter';
@@ -55,7 +55,42 @@ const StockChart = () => {
 
   return (
     <div className={styles.container}>
-        <ul className='z-40'>
+              <ul className='flex absolute top-80 ml-20 left-1/3 z-40'>
+            {Object.keys(chartConfig).map((item) => {
+                return (
+                    <li key={item}>
+                        <ChartFilter text={item} active={filter === item} onClick={() => {
+                            setFilter(item)
+                        }} />
+                    </li>
+                )
+            })}
+        </ul>
+        <ResponsiveContainer>
+            <AreaChart data={data}>
+                <defs>
+                    <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor={'#BB86FC'} stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor={'#BB86FC'} stopOpacity={0}/>
+                    </linearGradient>
+                </defs>
+                <Area 
+                    type='monotone' 
+                    dataKey='value' 
+                    stroke='#312e81' 
+                    fillOpacity={1}
+                    strokeWidth={0.5}
+                    fill='url(#chartColor)'
+                />
+                <Tooltip 
+                    contentStyle={{backgroundColor: '#111827'}}
+                    itemStyle={{color:'#818cf8'}}
+                />
+                <XAxis dataKey={"date"} />
+                <YAxis domain={["dataMin", "dataMax"]} />
+            </AreaChart>
+        </ResponsiveContainer>
+        {/* <ul className='z-40'>
           {Object.keys(chartConfig).map((item) => {
             return (
               <li key={item}>
@@ -76,7 +111,7 @@ const StockChart = () => {
           <CartesianGrid stroke="var(--accent-2-color)" strokeDasharray="5 5" />
           <Bar dataKey="price" fill="var(--a-color)" barSize={30} />
         </BarChart>
-      </div>
+      </div> */}
     </div>
   );
 };
