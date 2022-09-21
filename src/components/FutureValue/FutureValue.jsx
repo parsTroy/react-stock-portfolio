9297e6f458401fae763176dad0f307408030adab
parsTroy <troyparsons@hotmail.com>
2022-09-20 20:06:32 -0400

    foxied styling and graph data further

--- src/components/FutureValue/FutureValue.jsx
+++ src/components/FutureValue/FutureValue.jsx
@@ -7,46 +7,32 @@ const FutureValue = () => {
 
     const data = [
         {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
+          name: '2023',
+          uv: 10000,
         },
         {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
+          name: '2024',
+          uv: 11450,
         },
         {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
+          name: '2025',
+          uv: 12570,
         },
         {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
+          name: '2026',
+          uv: 13780,
         },
         {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
+          name: '2027',
+          uv: 15890,
         },
         {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
+          name: '2028',
+          uv: 19390,
         },
         {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
+          name: '2029',
+          uv: 23490,
         },
       ];
     
