11b6fb9fd0a85ddbabba3b4b885d78d3e6af1fb0
parsTroy <troyparsons@hotmail.com>
2022-09-29 20:58:44 -0400

    easteregg

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import styles from './ProfilePage.module.css';
 
 const Profile = () => {
+
+
+  // Fizz Buzz Checks
+  // for (let i = 0; i <= 100; i++) {
+    
+  //   let printed = '';
+
+  //   if (i % 3 === 0)
+  //     printed += 'Fizz';
+  //   if (i % 5 === 0)
+  //     printed += 'Buzz';
+  //   if (i % 7 === 0)
+  //     printed += 'Bazz';
+
+  //   console.log(printed || i);
+  // }
+
+
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -9,6 +27,7 @@ const Profile = () => {
           <div className="grid grid-cols-2">
             <div className="mr-10">
               Left Side
+              <div></div>
             </div>
             <div className="ml-10">
               Right Side
